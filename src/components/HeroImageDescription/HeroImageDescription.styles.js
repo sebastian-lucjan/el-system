daf561f8016a7053e2c73d139e2e7b4fb4c32544
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-08-03 12:41:40 +0200

    Add prop-type package, propTypes to Title component

--- src/components/HeroImageDescription/HeroImageDescription.styles.js
+++ src/components/HeroImageDescription/HeroImageDescription.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StyledHeroImageDescription = styled.div`
   padding-top: 30px;
   width: 75%;
-  font-size: ${({ theme }) => theme.fontSize.l};
+  font-size: ${({ theme }) => theme.fontSize.m};
   letter-spacing: 0.5px;
   font-weight: 300;
 `;
