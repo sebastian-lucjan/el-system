60f58f044ca7fec8e87496b20810462c1eef5329
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-08-03 07:56:05 +0200

    Start About company subpage

--- src/components/views/Root/Root.js
+++ src/components/views/Root/Root.js
@@ -4,12 +4,14 @@ import { theme } from '../../../assets/styles/theme';
 import StartPage from '../StartPage';
 import GlobalStyle from '../../../assets/styles/globalStyles';
 import { Wrapper } from './Root.styles';
+import About from '../../About/About';
 
 const Root = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Wrapper>
       <StartPage />
+      <About />
     </Wrapper>
   </ThemeProvider>
 );
@@ -17,7 +19,7 @@ const Root = () => (
 export default Root;
 
 // TODO: zrobić placeholder dla Slidera zeby "skonczyc" stronę startową
-// TODO: merge firs-page branch to main
+// DONEz: merge firs-page branch to main
 // TODO: zrobić "O firmie"
 // TODO: wydzielić komponent Button w nawigacji, dodać ::after z mini animacją i cursorem pointerem
 // TODO: sciagnac wszystkie ikony potrzebne do zrobienia dzialu oferta
