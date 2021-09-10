cbe6d7d4bf58f9c1e647249ccf5ae0c1596667ce
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-09-10 15:24:43 +0200

    Add lazy Suspense to page views

--- src/providers/MainTemplate.js
+++ src/providers/MainTemplate.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import { size } from '../assets/styles/theme';
 import Header from '../components/Header/Header';
 import StartPage from '../views/StartPage/StartPage';
-import AboutPage from '../views/AboutPage/AboutPage';
-import Offer from '../views/Offer/Offer';
-import Cooperation from '../views/Cooperation/Cooperation';
-import Contact from '../views/Contact/Contact';
-import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
+// import AboutPage from '../views/AboutPage/AboutPage';
+// import Offer from '../views/Offer/Offer';
+// import Cooperation from '../views/Cooperation/Cooperation';
+// import Contact from '../views/Contact/Contact';
+// import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 import Cookies from '../components/Cookies/Cookies';
 import { Wrapper } from '../components/Root/Root.styles';
+
+const AboutPage = lazy(() => import('../views/AboutPage/AboutPage'));
+const Offer = lazy(() => import('../views/Offer/Offer'));
+const Cooperation = lazy(() => import('../views/Cooperation/Cooperation'));
+const Contact = lazy(() => import('../views/Contact/Contact'));
+const ScrollToTop = lazy(() => import('../components/ScrollToTop/ScrollToTop'));
+// const Cookies = lazy(() => import('../components/Cookies/Cookies'));
 
 const borderMediaValue = size.width.md;
 
@@ -67,11 +74,13 @@ const MainTemplate = () => {
         activeMobileNavigation={activeMobileNavigation}
         handleChangeActiveMobileNav={handleChangeActiveMobileNav}
       />
-      <AboutPage name="about" />
-      <Offer name="offer" mobile={visibleHamburger} />
-      <Cooperation name="cooperation" />
-      <Contact name="contact" mobile={visibleHamburger} />
-      {pageY > 100 && <ScrollToTop mobile={visibleHamburger} to="" />}
+      <Suspense fallback={<div>loading</div>}>
+        <AboutPage name="about" />
+        <Offer name="offer" mobile={visibleHamburger} />
+        <Cooperation name="cooperation" />
+        <Contact name="contact" mobile={visibleHamburger} />
+        {pageY > 100 && <ScrollToTop mobile={visibleHamburger} to="" />}
+      </Suspense>
       {cookiesPopUp && (
         <Cookies
           handleCookiesA
