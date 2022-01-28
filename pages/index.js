600f55d0e88335f421c1d0e1f3bf2e4580947341
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-28 12:48:35 +0100

    Add title to '/' page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import MainTemplate from 'providers/MainTemplate';
+import { Head } from 'next/document';
 
 export default function Root() {
   return (
     <>
+      <Head>
+        <title>elsystem | usługi z branży elektroenergetycznej</title>
+      </Head>
       <MainTemplate />
     </>
   );
