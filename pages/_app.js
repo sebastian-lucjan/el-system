c9e26b844219f22a2051e188e6b6a5e9f7302cb1
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-28 12:11:21 +0100

    Small change#3 to improve performance from 99 - fonts optimization, Add meta and link tags to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/globalStyles';
 import { theme } from 'styles/theme';
-import 'styles/fonts.scss';
+// import 'styles/fonts.scss';
 
 export default function App({ Component, pageProps }) {
   return (
