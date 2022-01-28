d8cd777cc304cc6104ae2a21deb6892f85478a15
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-28 12:27:14 +0100

    Delete unnecessary font weights from link

--- pages/_document.js
+++ pages/_document.js
@@ -78,7 +78,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;500;700&family=Work+Sans:wght@200;300;400;500;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;700&family=Work+Sans:wght@200;300;400;600&display=swap"
             rel="stylesheet"
           />
           <meta name="robots" content="noindex" />
