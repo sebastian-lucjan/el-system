0c70cfda590f314c979c5595df4e733d9d3c7031
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-28 12:33:50 +0100

    Add title into Head component in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/globalStyles';
 import { theme } from 'styles/theme';
+import { Head } from 'next/document';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>elsystem | usługi z branży elektroenergetycznej</title>
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
