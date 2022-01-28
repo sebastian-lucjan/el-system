481f2f03a8a903d5cc288cddeba41033583023cb
sebastian-lucjan <sebastian.lucjan@gmail.com>
2022-01-28 12:40:29 +0100

    Fix unused import during buildtime

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/globalStyles';
 import { theme } from 'styles/theme';
-import { Head } from 'next/document';
 
 export default function App({ Component, pageProps }) {
   return (
